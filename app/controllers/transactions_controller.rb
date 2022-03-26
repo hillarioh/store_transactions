38b6befb9af200c5df57c6d9a454b162f934576d
hillarioh <okerioh@gmail.com>
2022-03-26 07:17:05 +0300

    setup controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,31 @@
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show]
+
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
+
+  def show
+    render json: @transaction
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      render json: @transaction, status: :created, location: @transaction
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    def transaction_params
+      params.require(:transaction).permit(:customer_id, :amount, :currency_from, :currency_to, :date_of_transaction)
+    end
+end
