b6ed0ba955f1b9e7e993bbc723a4541c645600f4
hillarioh <okerioh@gmail.com>
2022-03-26 07:49:56 +0300

    configure route and debug

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,3 @@
 class Transaction < ApplicationRecord
-    validates :username, :customer_id, :amount, :currency_from, :currency_to, :date_of_transaction, :presence=> true
+    validates :customer_id, :amount, :currency_from, :currency_to, :date_of_transaction, :presence=> true
 end
