827916889e81b002acd8c0ea8ea7e5778e46e5cd
hillarioh <okerioh@gmail.com>
2022-03-29 16:07:37 +0300

    add state column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_062343) do
+ActiveRecord::Schema.define(version: 2022_03_29_130325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_03_26_062343) do
     t.datetime "date_of_transaction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "state", default: "initial"
   end
 
   create_table "users", force: :cascade do |t|
